55365c1f69897a37809425422d64ff7615ff0cb7
Ramakrishna545 <ramakrishnapandiboina@gmail.com>
2023-02-23 17:59:14 +0530

    rectangle component created and added functionality

---
+++ src/app/rectangle/rectangle.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rectangle',
+  templateUrl: './rectangle.component.html',
+  styleUrls: ['./rectangle.component.css']
+})
+export class RectangleComponent {
+  public number1:number = 0;
+  public number2:number =0;
+  public result: number= 0;
+
+  area(){
+    this.result=this.number1*this.number2
+  }
+  perimeter(){
+    this.result= 2 * this.number1 + this.number2
+  }
+  
+
+}
