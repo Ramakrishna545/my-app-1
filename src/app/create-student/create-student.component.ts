6631a04bc7ab01ff88203300f18e28955e3b5ed6
Ramakrishna545 <ramakrishnapandiboina@gmail.com>
2023-03-13 12:13:09 +0530

    student form added form validations

--- src/app/create-student/create-student.component.ts
+++ src/app/create-student/create-student.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-student',
@@ -10,13 +10,13 @@ export class CreateStudentComponent {
 
   public createStudent:FormGroup= new  FormGroup(
     {
-      name: new FormControl(),
-      age: new FormControl(),
-      email: new FormControl(),
+      name: new FormControl("",[Validators.required,Validators.minLength(3),Validators.maxLength(15)]),
+      age: new FormControl("",[Validators.required,Validators.min(0),Validators.max(100)]),
+      email: new FormControl("",[Validators.required,Validators.email]),
       address: new FormGroup(
         {
-          state: new FormControl(),
-          pin: new FormControl()
+          state: new FormControl("",[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
+          pin: new FormControl("",[Validators.required,Validators.min(100000),Validators.max(999999)])
         }
       ),
       type: new FormControl(),
@@ -34,9 +34,9 @@ export class CreateStudentComponent {
     this.CardsFormArray.push(
       new FormGroup(
         {
-          number: new FormControl(),
+          number: new FormControl("",[Validators.required,Validators.min(100000000000),Validators.max(999999999999)]),
           expiry: new FormControl(),
-          cvv: new FormControl()
+          cvv: new FormControl("",[Validators.required,Validators.min(100),Validators.max(999)])
         }
       )
 
